b0b587f11fd2f5ffa6e96131e810ce0b92e255d6
Mykola Melnyk <mr.nicholas.x@gmail.com>
2022-10-14 22:44:33 +0300

    second commit

---
+++ src/main/java/com/example/libraryspring_mvc/book/Title.java
@@ -0,0 +1,18 @@
+package com.example.libraryspring_mvc.book;
+
+public class Title {
+    private String titleValue;
+    public Title(){
+    }
+
+    public Title(String titleValue){
+        this.titleValue = titleValue;
+    }
+
+    public String getTitleValue() {
+        return titleValue;
+    }
+    public void setTitleValue(String titleValue) {
+        this.titleValue = titleValue;
+    }
+}
---
+++ src/main/java/com/example/libraryspring_mvc/controller/ListBooksController.java
@@ -0,0 +1,24 @@
+package com.example.libraryspring_mvc.controller;
+
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.example.libraryspring_mvc.book.Book;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+public class ListBooksController extends AbstractController {
+
+    private List<Book> books;
+    @Override
+    protected ModelAndView handleRequestInternal(HttpServletRequest request,
+                                                 HttpServletResponse response) throws Exception {
+        return new ModelAndView("listBooks","books",books);
+    }
+    public List<Book> getBooks() {
+        return books;
+    }
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
+}
